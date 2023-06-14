df0787c1fcccd132c9737a82d4840bd0335cebce
kokoball <kbp4154@naver.com>
2023-06-14 22:43:48 +0900

    client/server component, import 추가

---
+++ fresh/app/cart/page.js
@@ -0,0 +1,27 @@
+import {age, name} from './data.js'
+
+export default function Cart() {
+    return (
+        <div>
+            <h4 className="title">Cart</h4>
+            <div className="cart-item">
+                <p>상품명 {age}</p>
+                <p>$40</p>
+                <p>1개</p>
+            </div>
+            <CartItem/>
+            <CartItem/>
+            <CartItem/>
+        </div>
+    )
+} 
+
+function CartItem() {
+    return (
+        <div className="cart-item">
+            <p>상품명</p>
+            <p>$40</p>
+            <p>1개</p>
+        </div>
+    )
+}
--- fresh/app/list/page.js
+++ fresh/app/list/page.js
@@ -1,13 +1,18 @@
 export default function List() {
+    let 상품 = ['Tomatoes', 'Pasta', 'Coconut']
     return (
         <div>
             <h4 className='title'>상품목록</h4>
-            <div className="food">
-                <h4>상품 1 $50</h4>
-            </div>
-            <div className="food">
-                <h4>상품 2 $40</h4>
-            </div>
+            {
+                상품.map((item, i) => {
+                    return (
+                        <div className="food" key={i}>
+                            <img src={`/food${i}.png`} className="food-img" />
+                            <h4>{item} $50</h4>
+                        </div>)
+                })
+            }
+            
         </div>
     )
 }
