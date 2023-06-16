0facd7f300ffbcbf3ddbee7745633b3986d825f2
kokoball <kbp4154@naver.com>
2023-06-16 23:03:16 +0900

    useRouter 추가

---
+++ forum/app/list/DetailLink.js
@@ -0,0 +1,12 @@
+'use client'
+
+import { useRouter } from "next/navigation"
+
+export default function DetailLink() {
+    let router = useRouter()
+    // let a = usePathname();
+    // let b = useSearchParams();
+    return (
+        <button onClick={() => {router.push('/')}}>버튼</button>
+    )
+}
