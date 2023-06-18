a96a198f49d3ff05786324d835086837d1b3df32
kokoball <kbp4154@naver.com>
2023-06-18 19:02:11 +0900

    cache 기능 추가

--- forum/app/list/ListItem.js
+++ forum/app/list/ListItem.js
@@ -12,8 +12,15 @@ export default function ListItem(props) {
                             <h4>{a.title}</h4>
                         </Link>
                         <Link href={'/edit/' + props.result[i]._id}>수정</Link>
-                        <span onClick={() => {
+                        <span onClick={(e) => {
                             fetch('/api/post/delete', { method: 'POST', body: props.result[i]._id })
+                                .then((r) => r.json())
+                                .then(() => {
+                                    e.target.parentElement.style.opacity = 0;
+                                    setTimeout(() => {
+                                        e.target.parentElement.style.display = 'none';
+                                    }, 1000)
+                                })
                         }}>삭제</span>
                         <p>1월 1일</p>
                     </div>
--- forum/app/page.jsx
+++ forum/app/page.jsx
@@ -1,10 +1,14 @@
 import { connectDB } from '@/util/database'
 
+export const revalidate = 60; // @NOTE: 60 초 동안 캐싱
+
 export default async function Home() {
 
   const db = (await connectDB).db('forum')
   let result = await db.collection('post').find().toArray();
   console.log(result)
+
+  await fetch('/URL', { cache : 'no-store' })
 
   return (
     <div>안녕</div>
