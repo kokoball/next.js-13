b215ac813267057e127ab1a0fbb5da6ee4efdf13
kokoball <kbp4154@naver.com>
2023-06-25 23:10:18 +0900

    caching 추가

--- forum/app/page.jsx
+++ forum/app/page.jsx
@@ -1,6 +1,6 @@
 import { connectDB } from '@/util/database'
 
-export const revalidate = 60; // @NOTE: 60 초 동안 캐싱
+// export const revalidate = 60; // @NOTE: 60 초 동안 캐싱, 페이지 단위로 캐싱
 
 export default async function Home() {
 
@@ -8,7 +8,10 @@ export default async function Home() {
   let result = await db.collection('post').find().toArray();
   console.log(result)
 
-  await fetch('/URL', { cache : 'no-store' })
+  // await fetch('/URL', { cache : 'force-cache' }) // 캐싱, 뒤에 파라미터 없어도 ㄱㅊ
+  // await fetch('/URL', { cache: 'no-store' }) // 캐싱 안함, 실시간 데이터 중요할때
+  // await fetch('/URL', {next : {revalidate: 60}}) // 60 초마다 캐싱 
+  
 
   return (
     <div>안녕</div>
